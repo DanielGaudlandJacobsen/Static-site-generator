eb2e760121269f033b54f139499e7309648a97d8
DanielGaudlandJacobsen <djacobsen5@gmail.com>
2025-05-23 22:46:56 +0200

    add ability to deplay website on github

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 import os
 import shutil
+import sys
 
 from static_mover import static_mover
 from page_generator import generate_pages_recursive
 
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
+else:
+    basepath = "/"
 
 current_path = os.path.abspath(__file__)
 current_dir = os.path.dirname(current_path)
 source = os.path.join(current_dir, "..", "static")
 source_path = os.path.abspath(source)
-destination = os.path.join(current_dir, "..", "public")
+destination = os.path.join(current_dir, "..", "docs")
 destination_path = os.path.abspath(destination)
 template = os.path.join(current_dir, "..")
 template_path = os.path.abspath(template)
@@ -18,7 +23,11 @@ content_path = os.path.abspath(content)
 
 
 def main():
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
     static_mover(source_path, destination_path)
-    generate_pages_recursive(content_path, template_path, destination_path)
+    generate_pages_recursive(content_path, template_path, destination_path, basepath)
 
 main()
--- src/page_generator.py
+++ src/page_generator.py
@@ -10,24 +10,27 @@ def extract_title_markdown(markdown):
     raise ValueError("No title in document")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     md = open(os.path.join(from_path)).read()
     template = open(os.path.join(template_path, "template.html")).read()
     html = markdown_to_html_node(md).to_html()
     title = extract_title_markdown(md)
-    template = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
     index = open(os.path.join(dest_path, "index.html"), "w")
     index.write(template)
     index.close()
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         file_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(file_path):
             os.makedirs(dest_dir_path, exist_ok=True)
-            generate_page(file_path, template_path, dest_dir_path)
+            generate_page(file_path, template_path, dest_dir_path, basepath)
         elif os.path.isdir(file_path):
-            generate_pages_recursive(file_path, template_path, dest_path)
+            generate_pages_recursive(file_path, template_path, dest_path, basepath)
